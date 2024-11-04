620fb0fb8728439c6e6d8c5031895eda1a355884
ldco2016 <renaissance.scholar2012@gmail.com>
2024-11-03 22:38:29 -0600

    Project Wrapup

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,20 @@ impl Account {
             balance: 0,
         }
     }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
+    }
+
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32 {
+        self.balance -= amount;
+        self.balance
+    }
 }
 
 #[derive(Debug)]
@@ -24,8 +38,33 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts
+            .iter()
+            .map(|account| account.summary())
+            .collect::<Vec<String>>()
+    }
 }
 
 fn main() {
-    println!("Hello, world!");
+    // do not forget to make bank variable mutable
+    let mut bank = Bank::new();
+    let mut account = Account::new(1, String::from("me"));
+
+    account.deposit(500);
+    account.withdraw(250);
+
+    bank.add_account(account);
+
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
